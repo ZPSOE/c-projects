c578ee3aa00fb156c34e17a2eaaa950d0f32fd33
ZPSOE <ZPSOE@users.noreply.github.com>
2026-09-15 22:47:41 +0800

    加入第一个 C 程序 Hello.c 和 .gitignore

---
+++ Hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+    char name[50];
+    int age;
+    printf("请输入你的名字和年龄：");
+    scanf("%s%d",name,&age);
+    printf("你好%s，你%d岁了",name,age);
+    return 0;
+}
